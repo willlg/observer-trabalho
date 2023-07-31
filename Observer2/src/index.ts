b644a042031a36126833ac523b08a11640df8cdc
willian <128076027+willlg@users.noreply.github.com>
2023-07-30 23:45:45 -0300

    Update index.ts

--- Observer2/src/index.ts
+++ Observer2/src/index.ts
@@ -12,11 +12,11 @@ Time.adicionarTorcedor(torcedor1);
 const torcedor2 = new TorcedorCabuloso('Arrascaeta', Time);
 Time.adicionarTorcedor(torcedor2);
 
-const torcedor3 = new TorcedorCule('Messi', Time2);
-Time2.adicionarTorcedor(torcedor3);
+//const torcedor3 = new TorcedorCule('Messi', Time2);
+//Time2.adicionarTorcedor(torcedor3);
 
-const torcedor4 = new TorcedorCule('Iniesta', Time2);
-Time2.adicionarTorcedor(torcedor4);
+//const torcedor4 = new TorcedorCule('Iniesta', Time2);
+//Time2.adicionarTorcedor(torcedor4);
 
 Time.marcarGol();
 Time.marcarGol();
@@ -25,9 +25,9 @@ Time.removerTorcedor(torcedor2);
 
 Time.marcarGol();
 
-Time2.marcarGol();
-Time2.marcarGol();
+//Time2.marcarGol();
+//Time2.marcarGol();
 
-Time2.removerTorcedor(torcedor4);
+//Time2.removerTorcedor(torcedor4);
 
-Time2.marcarGol();
+//Time2.marcarGol();
